f94742c383810c5fca8631dd8a2d8314efaec7b4
Luis Angel Dilone <ldilone@aclanyc.com>
2017-05-04 00:16:05 +0000

    Make it work

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
+    
+var students= ["Luis", "Jessie", "Christian", "Kiara", "Sammy",];
+$("#sAddButton").click(function(){
+var addStudent = $("#studentName").val();
+students.push(addStudent);
+});
 
+var teachers= ["Zack", "Aaron", "Alyxe", "Justin", "Julia"];
+$("#tAddButton").click(function(){
+var addTeacher = $("#teacherName").val();
+teachers.push(addTeacher);
+});
 
+$("#studentButton").click(function(){
+var random_s=Math.floor(Math.random()*students.length);
+$("#studentDisplay").html(students[random_s]);
+});
 
+$("#teacherButton").click(function(){
+var random_t=Math.floor(Math.random()*teachers.length);
+$("#teacherDisplay").html(teachers[random_t]);
+});
 });
